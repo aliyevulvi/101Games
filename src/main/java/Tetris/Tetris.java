ed22d0412465d0f8257a2882768e17be9a9a3149
Aliyew <aliyevulvi0012@gmail.com>
2025-09-29 20:06:08 +0300

    Tetris Block Action Updated

--- src/main/java/Tetris/Tetris.java
+++ src/main/java/Tetris/Tetris.java
@@ -2,7 +2,10 @@ package Tetris;
 
 import java.io.IOException;
 public class Tetris {
-     public static int[][] block = {{1,1},{1,1}};
+     public static int[][] block2 = {{1,1},{1,1}};
+     public static int[][] block = {{1,0},{1,0},{1,1}};
+     public static int[][] block3 = {{0,1},{0,1},{1,1}};
+
 	private static int[][] board = new int[20][10];
 	private static int row = 20;
 	private static int column = 10;
@@ -10,33 +13,51 @@ public class Tetris {
     public static void main(String args[]) throws IOException{
 		createBoard();
 		putBoard();
+		moveBlock('d');
+		moveBlock('d');
+		// downBlock();
+		// downBlock();
+		moveBlock('d');
+		moveBlock('d');
+		moveBlock('d');
+		printBoard();
 		
-		int i = 0;
 		while (!checkCollision()){
 			
-			if (System.in.available() > 0){
-			    int c = System.in.read();
-			    char ch = (char) c;
-			    
-			    if (ch == 'q'){
-			        System.out.println("Exit");
-			        break;
-			    }
-			}
 			
+
 			System.out.print("\033[H\033[2J");
             System.out.flush();
 			
 			downBlock();
+
+			if (System.in.available() > 0){
+			    int c = System.in.read();
+			    char ch = (char) c;
+			    
+			    if (ch == 'a' || ch =='d'){
+			        moveBlock(ch);
+			    } else if (ch == 'q'){
+					break;
+				}
+
+			}
+			System.out.print("\033[H\033[2J");
+            System.out.flush();
+
 			printBoard();
+
 			
 			try {
-				Thread.sleep(2000);
+				Thread.sleep(300);
 			} catch (InterruptedException e){
 				e.printStackTrace();
 			}
 			if (checkCollision())
 			    add2Board();
+			
+			
+			
 		}
 		
 		
@@ -45,11 +66,29 @@ public class Tetris {
         
     }
 	
+	private static void moveBlock(char ch) {
+
+		if (ch == 'd'){
+			for (int i = 0; i < board.length; i++) {
+				for (int j = board[0].length-1; j >= 0 ; j--) {
+					if (board[i][j] == 1 && j < board[0].length-1){
+						board[i][j] = 0;
+						board[i][j+1] = 1;
+					}
+				}
+			}
+		} else if (ch == 'a'){
+
+		}
+
+
+	}
+
 	public static void createBoard(){
 		
 	
-		for (int i = 0; i < 20; i++){
-			for (int j = 0; j < 10; j++){
+		for (int i = 0; i < row; i++){
+			for (int j = 0; j < column; j++){
 				board[i][j] = 0;
 			}
 		}
@@ -59,10 +98,10 @@ public class Tetris {
 	
 	public static void printBoard(){
 		
-		for (int i = 0; i < 20; i++){
+		for (int i = 0; i < row; i++){
 			
 			System.out.print("| ");
-			for (int j = 0; j < 10; j++){
+			for (int j = 0; j < column; j++){
 				if (board[i][j] == 0){
 					System.out.print("- ");
 				} else 
@@ -73,7 +112,7 @@ public class Tetris {
 		}
 		
 		System.out.print("  ");
-		for (int i = 0; i<10; i++){
+		for (int i = 0; i<column; i++){
 			System.out.print("X ");
 		}
 		
@@ -82,11 +121,10 @@ public class Tetris {
 	
 	
 	public static void putBoard(){
-		
-		for (int i = 0; i<block[0].length; i++){
-			for (int j = 3; j < block[0].length+3; j++){
-				board[i][j] = block[i][j-3];
-				//System.out.print(board[i][j]);
+
+		for (int i = 0; i<block.length; i++){
+			for (int j = 0; j < block[0].length; j++){
+				board[i][j] = block[i][j];
 			}
 		}
 		
@@ -95,8 +133,8 @@ public class Tetris {
 	
 	public static void downBlock(){
 		
-		for (int i = 18; i>=0; i--){
-			for (int j = 0; j < 10; j++){
+		for (int i = row-2; i>=0; i--){
+			for (int j = 0; j < column; j++){
 				if (board[i][j] == 1){
 					board[i][j]=0;
 					board[i+1][j]=1;
