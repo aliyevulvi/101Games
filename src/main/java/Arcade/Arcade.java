bfba51dde9cf56d9c10a22bf8298fd8af7ca2551
aliyew <aliyevulvi0012@gmail.com>
2025-10-13 09:23:29 +0300

    Arcade Completed

--- src/main/java/Arcade/Arcade.java
+++ src/main/java/Arcade/Arcade.java
@@ -1,7 +1,8 @@
 package Arcade;
 
 import java.io.IOException;
-import java.time.*;
+import java.time.LocalTime;
+import java.time.Duration;
 
 public class Arcade {
 	private static int row = 20;
@@ -10,7 +11,8 @@ public class Arcade {
 	private static int score = 0;
 	private static int x = 15, y = 5;
 	private static boolean isGameOver = false;
-	private static int fire = 4, second = 1000, level = 1;
+	private static int fire = 4, second = 1000, level = 1, failure = 0, enemyAction = 0;
+	private static long totalSeconds = 0,  minutes = 0, seconds = 0;
 	
 	public static void main(String[] args) throws IOException {
 		startGame();
@@ -19,12 +21,17 @@ public class Arcade {
 	public static void startGame() throws IOException {
 	    createBoard();
 		createEnemies(level);
+		LocalTime start = LocalTime.now();
 	    
 	    while (!isGameOver){
 			
-
-	        System.out.print("\033[H\033[2J");
+            System.out.print("\033[H\033[2J");
             System.out.flush();
+        
+	        
+            
+            bulletAction();
+	        
 
             if (fire == 0) {
                 fire = second/250;
@@ -32,8 +39,14 @@ public class Arcade {
             } else 
                 fire--;
                 
-	        bulletAction();
-	        printBoard();
+	        if (enemyAction == 0){
+	            enemyAction = second/200;
+	            enemyAction();
+	        } else {
+	            enemyAction--;
+	        }
+	        
+	       printBoard();
 	        
 	        try {
 				Thread.sleep(100);
@@ -45,28 +58,126 @@ public class Arcade {
                 int c = System.in.read();
                 char ch = (char) c;
                 
-                if (ch == 'q'){
+                if (ch == 'Q'){
                     break;
                 } else if (ch == 'a' || ch == 'd'){
                     move(ch);
+                } else if (ch == 'f'){
+                    pause();
                 }
 			}
 			
 			while (System.in.available() > 0) {
                 System.in.read();
             }
-			
-			if (level > 3){
-				System.out.println("You Win!");
-				break;
-			} else if (noEnemy()){
+		
+		    if (noEnemy() && level < 3){
 				level++;
-				createEnemies(level);
 			}
+			
+			if (noEnemy()){
+			    createEnemies(level);
+			
+			}
+			
+			if (failure != 0){
+			    failure(failure);
+			    isGameOver = true;
+			}
+			
+			LocalTime end = LocalTime.now();
+			Duration dur = Duration.between(start, end);
+			totalSeconds = dur.getSeconds();
+			minutes = totalSeconds / 60;
+			seconds = totalSeconds % 60;
+			
                 
+               
+               
+               
                 
 			
 	    }
+	    
+	    
+	}
+	
+	private static void pause() throws IOException {
+	    
+	    while (true){
+	        
+	        System.out.println("--------------------");
+	        System.out.println("Press \"F\" For Resume");
+	        System.out.println("--------------------");
+	        
+	        if (System.in.available() > 0){
+                    int d = System.in.read();
+                    char pause = (char) d;
+                    
+                    if (pause == 'f')
+                        break;
+	        }
+	        
+	        try {
+				Thread.sleep(100);
+			} catch (InterruptedException e){
+				e.printStackTrace();
+			}
+			
+			System.out.print("\033[H\033[2J");
+            System.out.flush();
+	    }
+	}
+	
+	private static void playerStats(){
+	    
+	    System.out.println("--------Stats-------");
+	    System.out.println("Eliminated Enemies: " + (score/10) + " | Score: " + score);
+	    System.out.println("Survival Time: " + minutes + ":" + seconds);
+	    System.out.println("--------------------");
+	    
+	}
+	
+	private static void failure(int f){
+	    System.out.print("\033[H\033[2J");
+        System.out.flush();
+        
+	    String message = f == 1 ? "Enemies Killed You!" : "Enemies Passed You";
+	    System.out.println("--------------------");
+	    System.out.println("Game Over!");
+	    System.out.println(message);
+	    System.out.println("--------------------");
+	    System.out.println();
+	    
+	    playerStats();
+	    
+	}
+	
+	private static void enemyAction(){
+	    for (int i = row - 2; i >=0; i--){
+	        for (int j = 0; j < column; j++){
+	            if (board[i][j] == 3){
+	                if (board[i+1][j] == 0){
+	                    board[i+1][j] = 3;
+	                    board[i][j] = 0;
+	                    if (i == x)
+	                        failure = 2;
+	                } else if (board[i+1][j] == 2) {
+	                    board[i+1][j] = 0;
+	                    board[i][j] = 0;
+	                    score +=10;
+	                } else if (board[i+1][j] == 1){
+	                    board[i+1][j] = 0;
+	                    board[i][j] = 0;
+	                    failure = 1;
+	                } else if (board[i+1][j] == 3){
+	                    
+	                }
+	            }
+	        }
+	    }
+	    
+	    
 	}
 
 	private static void createEnemies(int level){
@@ -100,7 +211,7 @@ public class Arcade {
 	    
 	    if (ch == 'a'){
 	        for (int j = 1; j < column; j++){
-	            if (board[x][j] == 1) {
+	            if (board[x][j] == 1 && board[x][j-1] == 0) {
 	                board[x][j] = 0;
 	                board[x][j-1] = 1;
 	                y = j - 1;
@@ -110,7 +221,7 @@ public class Arcade {
 	        }
 	    } else if (ch == 'd') {
 	        for (int j = 0; j < column-1; j++){
-	            if (board[x][j] == 1) {
+	            if (board[x][j] == 1 && board[x][j+1] == 0) {
 	                board[x][j] = 0;
 	                board[x][j+1] = 1;
 	                y = j + 1;
@@ -136,7 +247,9 @@ public class Arcade {
 	private static void fire(){
 	    int bullet = 2;
 	    
-	    board[x-1][y] = bullet;
+	    if (board[x-1][y] == 0){
+	        board[x-1][y] = bullet;
+	    }
 	}
 	
 	private static void bulletAction() {
@@ -166,7 +279,7 @@ public class Arcade {
 	    System.out.println("\n");
 	    System.out.print("     ");
 	    for (int i = 0; i < column; i++){
-	        System.out.print(" V");
+	        System.out.print(" X");
 	    }
 	    System.out.println();
 	    
@@ -174,13 +287,13 @@ public class Arcade {
 	        System.out.print("    | ");
 	        for (int j = 0; j < column; j++){
 	            if (board[i][j] == 0){
-	                System.out.print("- ");
+	                System.out.print("  ");
 	            } else if (board[i][j] == 1) {
 	                System.out.print("A ");
 	            } else if (board[i][j] == 2){
 	                System.out.print(": ");
 	            } else if (board[i][j] == 3){
-	                System.out.print("X ");
+	                System.out.print("V ");
 	            }
 	            
 	        }
@@ -188,6 +301,7 @@ public class Arcade {
 	    }
 
 		System.out.println("\n[Score: " + score + "]");
+		System.out.println("[Duration: " + minutes + ":" + seconds + "]");
 	    
 	    
 	}
